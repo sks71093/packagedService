3568a5bdad9d2a55695f42a75f35352fce1458f5
Shivam Kumar Singh <shivam.kumar@girmiti.com>
2016-08-18 12:46:55 +0530

    no message

---
+++ sampleService2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace sampleService2
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Scheduler()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory +"\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ":" + ex.Source.ToString().Trim() + ";" + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        public static void WriteErrorLog(String message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory +"\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ":" + message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ sampleService2/Scheduler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Timers;
+
+
+namespace sampleService2
+{
+    public partial class Scheduler : ServiceBase
+    {
+        public int i;
+        private Timer timer1 = null;
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1 = new Timer();
+            this.timer1.Interval = 300000;
+            this.timer1.Elapsed += Timer1_Elapsed;
+            timer1.Enabled = true;
+            i = 1;
+            Program.WriteErrorLog("The timer been started");
+        }
+
+        private void Timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Program.WriteErrorLog(String.Format("Task {0} has been completed", i));
+            i++;
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Program.WriteErrorLog("Window Service has been Stopped");
+        }
+    }
+}
